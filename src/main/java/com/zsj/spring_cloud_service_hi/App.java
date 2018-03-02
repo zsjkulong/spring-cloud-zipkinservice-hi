c8d906805d734ff1985663bbe8ae9e9938ffdd8a
TSKY.sijian.zhou <zhousijian@travelsky.com>
2018-03-02 09:36:22 +0800

    Bug:233 Bugzilla Project: Sichuan Airline Description:

---
+++ src/main/java/com/zsj/spring_cloud_service_hi/App.java
@@ -0,0 +1,13 @@
+package com.zsj.spring_cloud_service_hi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
